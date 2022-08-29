73e541967033241b9bf9ac18d5d0d0b87a99d394
Punit Jajodia <pnt.jjd@gmail.com>
2022-08-29 15:37:06 +0545

    Fixed routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from "./Home";
+
+import './App.css';
+
+import { parse } from 'yaml';
+import { useState, useEffect } from 'react';
+import dbFile from "./data/db.yml";
+
 
 function App() {
+  const [db, setDb] = useState({});
+
+  useEffect(() => {
+    fetch(dbFile)
+    .then((r) => r.text())
+    .then(text  => {
+      setDb(parse(text));
+    })  
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+        <Route path="/" element={<Home db={db} setDb={setDb} />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,48 @@
+import logo from './logo.svg';
+import './App.css';
+import {useState} from 'react';
+
+
+function Home({db, setDb}) {
+
+  const [email, setEmail] = useState("");
+
+  const login = () => {
+
+    const account = db.admin_accounts.find(account => {
+      return account.email === email
+    });
+
+    if(account) {
+      setDb({
+        ...db,
+        current_login: {
+          email: email,
+          type: "employer"
+        }
+      })
+    } else {
+      alert("Oh no");
+    }
+  }
+
+  console.log(db.current_login);
+
+  if(db.current_login) {
+    return(
+      <div>Welcome back {db.current_login.email}</div>
+    )
+  }
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+         <input value={email} onChange={(e) => setEmail(e.target.value)}></input>
+         <button onClick = {login}>Login</button>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
