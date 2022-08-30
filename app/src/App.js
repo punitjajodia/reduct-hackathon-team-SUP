9608df8f1a2b484e4009affa435c70977420325b
Shrusha Raghubansi <you@example.com>
2022-08-30 15:27:42 +0545

    Results and Finish Page

--- app/src/App.js
+++ app/src/App.js
@@ -6,6 +6,8 @@ import CampaignList from "./Campaigns";
 import CreateCampaign from "./CreateCampaign";
 import InstructionPage from "./InstructionsPage";
 import Challenges from "./ChallengePage";
+import CampaignResults from "./CampaignResults";
+import Finish from "./Finish";
 
 
 import { parse } from "yaml";
@@ -32,13 +34,12 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home db={db} setDb={setDb} />} />
-          <Route
-            path="/campaigns"
-            element={<CampaignList db={db} setDb={setDb} loading={loading} />}
-          />
+          <Route path="/campaigns" element={<CampaignList db={db} setDb={setDb} loading={loading} />}/>
           <Route path="/create-campaign" element={<CreateCampaign challenges={db.challenges_list}/>} />
           <Route path="/instructions" element={<InstructionPage />} />
           <Route path="/challenge" element={<Challenges />} />
+          <Route path="/results" element={<CampaignResults />} />
+          <Route path="/finish" element={<Finish />} />
         </Routes>
       </BrowserRouter>
     </div>
